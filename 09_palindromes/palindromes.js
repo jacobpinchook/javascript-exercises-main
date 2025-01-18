7af59358e30e6c98956978ba61acbe4a18590005
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-18 12:44:31 -0500

    Palindromes exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //remove any non string/num
+    let original = str.toLowerCase().replaceAll(".", "").replaceAll("!", "").replaceAll(", ", "").replaceAll(" ", "");
+    //save the reversed string in an array
+    let reversedStr = original.split('').reverse().join('');
+    // //compare the arrays
+    if (reversedStr == original) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
