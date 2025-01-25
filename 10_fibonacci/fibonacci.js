4eadcfac01961cf1f2cc6848de516fa8bcbf413e
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-24 23:55:01 -0500

    Test one complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,15 @@
 // The Fibonacci sequence is a series of numbers where each number is the sum of the two numbers that come before it. The sequence starts with 0 and 1, and then continues with 1, 2, 3, 5, 8, 13, 21, and so on.
 const fibonacci = function() {
     // Use recursion
-
+    function fibonacci(n) {
+        if (n <= 1) {
+          return n;
+        } else {
+          return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+      }
+      
+      console.log(fibonacci(6)); // Output: 8
 };
 
 // Do not edit below this line
