daa24cf3e71c09a769f3d3d251c8dcb7a14d654b
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-26 22:47:31 -0500

    Add pseudo code

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
 const getTheTitles = function() {
-
+    // Find book titles
+    // Add title to new array
+    // Iterate until the array has been looked over completely
 };
 
 // Do not edit below this line
