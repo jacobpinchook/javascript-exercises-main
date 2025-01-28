e09d040b83e66d00e0d0593a04dca3cc33884c93
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-27 22:33:32 -0500

    Complete get the titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,7 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
     // Find book titles
+    const titles = arr.map((item) => item.title);
     // Add title to new array
     // Iterate until the array has been looked over completely
+    return titles;
 };
 
 // Do not edit below this line
