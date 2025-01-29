0ab2047bf3d653ae5273e7ca5e3bddcddb899e91
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-28 22:34:40 -0500

    Add pseudo code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
 const findTheOldest = function() {
+    // Calculate age for each person in the array of objects
 
+    // Compare the ages
+
+    // Return the name of the object with the highest age value
 };
 
 // Do not edit below this line
