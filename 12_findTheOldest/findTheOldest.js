6fecc9c76f044d358599f21a8152d6564b9b5c03
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-30 17:09:46 -0500

    First attempt at findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,9 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
     // Calculate age for each person in the array of objects
-
+    const updatedArr = arr.map((person) => (!person.yearOfDeath) ? person.age = (currentTime.getFullYear() - person.yearOfBirth) : person.age = (person.yearOfDeath - person.yearOfBirth));
     // Compare the ages
-
+    const sortedArr = arr.sort((a, b) => b.age - a.age);
     // Return the name of the object with the highest age value
+    return sortedArr[0];
 };
 
 // Do not edit below this line
