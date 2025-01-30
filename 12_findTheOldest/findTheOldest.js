856ab6ea35449e5995cc232da7d296f0299af20b
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-30 17:12:19 -0500

    Add dynamic date functionality

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
 const findTheOldest = function(arr) {
+    // Store current date
+    const d = new Date();
     // Calculate age for each person in the array of objects
-    const updatedArr = arr.map((person) => (!person.yearOfDeath) ? person.age = (2025 - person.yearOfBirth) : person.age = (person.yearOfDeath - person.yearOfBirth));
+    const updatedArr = arr.map((person) => (!person.yearOfDeath) ? person.age = (d.getFullYear() - person.yearOfBirth) : person.age = (person.yearOfDeath - person.yearOfBirth));
     // Compare the ages
     const sortedArr = arr.sort((a, b) => b.age - a.age);
     // Return the name of the object with the highest age value
